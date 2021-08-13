a78c8d6fc5ba19ef764b462010a0edd4167ff83a
ray1218 <raytham1218@gmail.com>
2021-08-13 08:29:07 +0800

    redone Q1a

--- archieve/OOP class/T1/Q1a.cpp
+++ archieve/OOP class/T1/Q1a.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 
 int main(void) {
-    std::cout << "MMU Fee Structure 2021\n"
-                 "   Programme      Duration        Fee\n"
+    std::cout << "MMU Fee Structure 2021\n";
+    std::cout << std::setw(12) << std::right;
+    std::cout << "   Programme      Duration        Fee\n"
                  "     Diploma       2 Years      23000\n"
-                 "      Degree       3 Years      60000\n"
-              << std::endl;
+                 "      Degree       3 Years      60000\n";
 }
